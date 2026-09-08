1bbdcac02608b5b404baafe39c47c77a55dbb0b3
Intended <neobrugman12@gmail.com>
2026-09-08 10:13:04 +0200

    Fixed db

--- Includes/Database.php
+++ Includes/Database.php
@@ -1,2 +1,9 @@
 <?php
-// Database connection disabled.
+
+$conn = new mysqli("localhost", "root", "", "camping_maple");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// fart
+?>
