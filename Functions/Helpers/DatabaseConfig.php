6c5bdefbac7d06bb7ecadb12318009b9770984d7
Intended <neobrugman12@gmail.com>
2026-09-16 10:25:51 +0200

    DatabaseConfig.php

---
+++ Functions/Helpers/DatabaseConfig.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+function maple_database_config(): array
+{
+    return [
+        'host' => 'localhost',
+        'database' => 'camping_maple',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8mb4',
+    ];
+}
